c7764afc9abc92faa5141b645f4ec4e88d25c540
ferro5 <sviridov288@gmail.com>
2024-07-07 11:41:32 +0300

    Add project files.

---
+++ src/Core/Abstractions/Entity.cs
@@ -0,0 +1,35 @@
+﻿namespace Core.Abstractions;
+
+public abstract class Entity
+{
+
+    private readonly List<IDomainEvent> _domainEvents = new();
+    public Guid Id { get; set; }
+
+    protected Entity()
+    {
+        
+    }
+
+    public Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public IReadOnlyCollection<IDomainEvent> GetAllDomainEvent()
+    {
+        return _domainEvents.ToList();
+    }
+    public void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvent()
+    {
+        _domainEvents.Clear();
+    }
+
+
+    
+}
